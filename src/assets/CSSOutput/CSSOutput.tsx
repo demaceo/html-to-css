1443e955173e80229b1ebde86857a137873f3811
demaceo <hdemaceo@gmail.com>
2024-10-30 16:33:48 -0600

    convert CSSOutput to tsx

---
+++ src/assets/CSSOutput/CSSOutput.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './CSSOutput.css'
+
+interface CSSOutputProps {
+  cssContent: string;
+}
+
+const CSSOutput: React.FC<CSSOutputProps> = ({ cssContent }) => (
+  <div className="output-container">
+    <label>
+      <strong id="cssTitle">Generated CSS:</strong>
+    </label>
+    <textarea
+      // rows="10"
+      value={cssContent}
+      readOnly
+      placeholder="Your generated CSS will appear here..."
+    />
+  </div>
+);
+
+CSSOutput.propTypes = {
+  cssContent: PropTypes.string.isRequired,
+};
+
+export default CSSOutput;
